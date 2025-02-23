3d2416e62d19edaebd0345e7df0ff90c8df275b4
ilan <ilanaaprilloriram@gmail.com>
2025-02-23 17:58:47 +0200

    added Explanation.txt + added more test

--- tests/test_folder_unique_names.py
+++ tests/test_folder_unique_names.py
@@ -6,11 +6,11 @@ class TestDifferentFoldersNames:
     # Parametrized fixture, single test is being executed per test parameter
     @pytest.mark.parametrize("folders_names_list",# <---- this is a name of the fixture, each time the fixture will hold 1 path to test by a test
                              [
-                                 # (["gta", "gta(1)", "gta(2)", "gta", "avalon"]),
-                                 # (["onepiece","onepiece(1)","onepiece(2)","onepiece(3)","onepiece"]),
-                                 # (["pes","fifa","gta","pes(2019)"]),
-                                 # (["gta"]),
-                                 # ([""]),
+                                 (["gta", "gta(1)", "gta(2)", "gta", "avalon"]),
+                                 (["onepiece","onepiece(1)","onepiece(2)","onepiece(3)","onepiece"]),
+                                 (["pes","fifa","gta","pes(2019)"]),
+                                 (["gta"]),
+                                 ([""]),
                                  ([])
                              ])
     def test_build_files_structure_and_return_longest_path(self,
